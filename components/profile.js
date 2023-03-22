a1ee1665416351b2b2591e8cb606b5c38d918701
Muhammadali1029 <104990048+Muhammadali1029@users.noreply.github.com>
2023-03-22 17:51:20 +0000

    Added get request for profile screen
    Details print to console but do not display on screen for some reason

--- components/profile.js
+++ components/profile.js
@@ -1,31 +1,109 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FlatList } from 'react-native-web';
 
 
-export default class ProfileScreen extends Component {
- 
+export default class ProfileScreen extends Component 
+{
+    constructor(props)
+    {
+        super(props);
+
+        this.state = 
+        {
+            isLoading: true,
+            profileData: []
+        };
+    }
+
+    componentDidMount()
+    {
+        this.getData();
+    }
+
+    getData = async () =>
+    {   
+        console.log("Profile request sent to api")
+        // userId = await AsyncStorage.getItem('whatsthat_user_id');
+        return fetch("http://localhost:3333/api/1.0.0/user/"+ (await AsyncStorage.getItem('whatsthat_user_id')),
+        {
+            method: 'get',
+            headers: 
+            {
+                'Content-Type': 'application/json',
+                'X-Authorization': await AsyncStorage.getItem('whatsthat_session_token')
+            }
+        })
+
+        .then((response) => response.json())
+        .then((responseJson) => 
+        {
+            console.log("Data returned from api");
+            console.log(responseJson);
+            this.setState
+            ({
+                isLoading: false,
+                profileData: responseJson
+            })
+        })
+        .catch((error) =>
+        {
+            console.log(error);
+        });
+    }
+
     render()
     {
-        return (
-            <View style={StyleSheet.container}>
-                <View style={StyleSheet.title}>
-                    <Text>Profile</Text>
+        if(this.state.isLoading)
+        {
+            return (
+                <View style={styles.container}>
+                    <ActivityIndicator />
                 </View>
+            )
+        }
 
-                <View style={StyleSheet.info}>
-                    <Text>Name</Text>
-                    <Text>Email</Text>
+        else
+        {
+            return (
+                // <View style={StyleSheet.container}>
+                //     <View style={StyleSheet.title}>
+                //         <Text>Profile</Text>
+                //     </View>
+
+                //     <View style={StyleSheet.info}>
+                //         <Text>Name</Text>
+                //         <Text>Email</Text>
+                //     </View>
+                    
+                //     <View style={StyleSheet.nav}>
+                //         <TouchableOpacity onPress={this._onPressButton}>
+                //             <View style={styles.button}>
+                //                 <Text style={styles.buttonText}>Edit</Text>
+                //             </View>
+                //         </TouchableOpacity>
+                //     </View>
+                // </View>
+
+                <View style={styles.container}>
+                    <Text>Profile Details</Text>
+                    <View>
+                        <FlatList
+                            data={this.state.profileData}
+                            renderItem = {(item) => 
+                            (
+                                <View>
+                                    <Text>{item.first_name} {item.last_name}</Text>
+                                    <Text>{item.email}</Text>
+                                </View>
+                            )}
+                            keyExtractor={({user_id}, index) => user_id}
+                        />
+                    </View>
                 </View>
-                
-                <View style={StyleSheet.nav}>
-                    <TouchableOpacity onPress={this._onPressButton}>
-                        <View style={styles.button}>
-                            <Text style={styles.buttonText}>Edit</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        )
+            )
+        }
     }
 
 }
@@ -33,7 +111,10 @@ export default class ProfileScreen extends Component {
 
 const styles = StyleSheet.create({
     container: {
-      width: "100%"
+        flex: 1,
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center"
     },
     title: {
        
