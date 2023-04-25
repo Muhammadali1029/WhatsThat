6c2ac6422dab33ee95b80ebb21b97031a3ea2308
Muhammadali1029 <104990048+Muhammadali1029@users.noreply.github.com>
2023-04-25 11:00:22 +0100

    added destructuring to blocked screen

--- components/blocked.js
+++ components/blocked.js
@@ -1,167 +1,176 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator} from 'react-native';
+import {
+  View, Text, TouchableOpacity, StyleSheet, ActivityIndicator,
+} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { FlatList } from 'react-native-web';
-import { Button } from 'react-native-web';
+import PropTypes from 'prop-types';
 
-
-
-export default class BlockedScreen extends Component 
+export default class BlockedScreen extends Component
 {
-    constructor(props)
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      blockedData: [],
+    };
+  }
+
+  componentDidMount()
+  {
+    this.getData();
+  }
+
+  componentDidUpdate(prevProps, prevState)
+  {
+    const { blockedData } = this.state;
+    if (prevState.blockedData.length !== blockedData.length)
     {
-        super(props);
-
-        this.state = 
-        {
-            isLoading: true,
-            blockedData: []
-        };
+      this.getData();
     }
+  }
 
-    componentDidMount()
-    {
-        this.getData();
-    }
-
-    componentDidUpdate(prevProps, prevState) 
-    {
-        if (prevState.blockedData.length !== this.state.blockedData.length) 
-        {
-          this.getData();
-        }
-    }
-
-    getData = async () =>
-    {   
-        console.log("Blocked request sent to api")
-        return fetch("http://localhost:3333/api/1.0.0/blocked",
-        {
-            method: 'get',
-            headers: 
+  getData = async () =>
+  {
+    console.log('Blocked request sent to api');
+    return fetch(
+      'http://localhost:3333/api/1.0.0/blocked',
+      {
+        method: 'get',
+        headers:
             {
-                'Content-Type': 'application/json',
-                'X-Authorization': await AsyncStorage.getItem('whatsthat_session_token')
-            }
-        })
-
-        .then((response) => response.json())
-        .then((responseJson) => 
-        {
-            console.log("Data returned from api");
-            console.log(responseJson);
-            this.setState
-            ({
-                isLoading: false,
-                blockedData: responseJson
-            });
-        })
-        .catch((error) =>
-        {
-            console.log(error);
+              'Content-Type': 'application/json',
+              'X-Authorization': await AsyncStorage.getItem('whatsthat_session_token'),
+            },
+      },
+    )
+      .then((response) => response.json())
+      .then((responseJson) =>
+      {
+        console.log('Data returned from api');
+        console.log(responseJson);
+        this.setState({
+          isLoading: false,
+          blockedData: responseJson,
         });
-    }
+      })
+      .catch((error) =>
+      {
+        console.log(error);
+      });
+  };
 
-    unblockUser = async (userID) =>
+  unblockUser = async (userID) => fetch(
+    `http://localhost:3333/api/1.0.0/user/${userID}/block`,
     {
-        return fetch("http://localhost:3333/api/1.0.0/user/"+ userID + "/block", 
+      method: 'DELETE',
+      headers:
         {
-            method: 'DELETE',
-            headers: 
-            {
-                'Content-Type': 'application/json',
-                'X-Authorization': await AsyncStorage.getItem('whatsthat_session_token')
-            }
-        })
+          'Content-Type': 'application/json',
+          'X-Authorization': await AsyncStorage.getItem('whatsthat_session_token'),
+        },
+    },
+  )
 
-        .then(async (response) => 
-        {
-            console.log("Unblock User request sent to api");
-            if(response.status === 200)
-            {   
-                console.log("User " + userID + " Unblocked")
-                this.getData();
-                this.props.route.params.getData();
-                this.props.route.params.removeFromContacts(userID);
-            }
-            else if(response.status === 400)
-            {
-                console.log("You cannot unblock yourself")
-            }
-            else if(response.status === 404)
-            {
-                console.log("User does not exist")
-            }
-            else
-            {
-                throw "Something went wrong"
-            }
-        })
-    }
-
-    render()
+    .then(async (response) =>
     {
-        if(this.state.isLoading)
-        {
-            return (
-                <View style={styles.container}>
-                    <ActivityIndicator />
-                </View>
-            )
-        }
+      const { route } = this.props;
+      const { params } = route;
 
-        else
-        {
-            return (
-                <View style={styles.container}>
-                    <Text>Blocked Users</Text>
-                    <View>
-                        <FlatList
-                            data={this.state.blockedData}
-                            renderItem = {({item}) => 
-                            (
-                                <View>
-                                    <Text>{item.first_name} {item.last_name}</Text>
-                                    <TouchableOpacity onPress={() => this.unblockUser(item.user_id)}>
-                                        <View style={styles.button}>
-                                            <Text style={styles.buttonText}>Unblock</Text>
-                                        </View>
-                                    </TouchableOpacity>
-                                </View>
-                            )}
-                            keyExtractor={({user_id}, index) => user_id}
-                        />
-                    </View>
-                </View>
-            )
-        }
+      console.log('Unblock User request sent to api');
+      if (response.status === 200)
+      {
+        console.log(`User ${userID} Unblocked`);
+        this.getData();
+        params.getData();
+        params.removeFromContacts(userID);
+      }
+      else if (response.status === 400)
+      {
+        console.log('You cannot unblock yourself');
+      }
+      else if (response.status === 404)
+      {
+        console.log('User does not exist');
+      }
+      else
+      {
+        throw 'Something went wrong';
+      }
+    });
+
+  render()
+  {
+    const { isLoading, blockedData } = this.state;
+    if (isLoading)
+    {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      );
     }
+
+    return (
+      <View style={styles.container}>
+        <Text>Blocked Users</Text>
+        <View>
+          <FlatList
+            data={blockedData}
+            renderItem={({ item }) => (
+              <View>
+                <Text>
+                  {item.first_name}
+                  {' '}
+                  {item.last_name}
+                </Text>
+                <TouchableOpacity onPress={() => this.unblockUser(item.user_id)}>
+                  <View style={styles.button}>
+                    <Text style={styles.buttonText}>Unblock</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            )}
+            // eslint-disable-next-line camelcase
+            keyExtractor={({ user_id }) => user_id}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
+BlockedScreen.propTypes = {
+  route: PropTypes.shape({
+    params: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    search: {
-       
-    },
-    info: {
-       
-    },
-    nav:{
-      marginBottom: 5
-    },
-    button: {
-      marginBottom: 30,
-      backgroundColor: '#2196F3'
-    },
-    buttonText: {
-      textAlign: 'center',
-      padding: 20,
-      color: 'white'
-    },
-  });
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  search: {
+
+  },
+  info: {
+
+  },
+  nav: {
+    marginBottom: 5,
+  },
+  button: {
+    marginBottom: 30,
+    backgroundColor: '#2196F3',
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white',
+  },
+});
